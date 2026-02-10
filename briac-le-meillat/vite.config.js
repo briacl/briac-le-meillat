9357f58e55e43d364831499c559ec1c38d6ee083
Briac LE MEILLAT <blemeill@briac.cf>
2026-02-10 18:29:31 +0100

    tentative 2 de correction pour déployer le site

--- briac-le-meillat/vite.config.js
+++ briac-le-meillat/vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
 export default defineConfig({
-    base: './', // Essential for GitHub Pages relative paths
+    base: '/briac-le-meillat/', // EXACT repository name with slashes
     plugins: [
         react(),
         VitePWA({
